9decdf56735c586b976abd97dac408d80e0d9b06
Artem Shumeiko <shumeykoartem@mail.ru>
2025-02-20 11:20:54 +0300

    add project structure

---
+++ src/api/books.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+from sqlalchemy import select
+
+from src.api.dependencies import SessionDep
+from src.database import engine, Base
+from src.models.books import BookModel
+from src.schemas.books import BookSchema, BookGetSchema
+
+router = APIRouter()
+
+
+@router.post("/setup")
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@router.post("/books")
+async def add_book(book: BookSchema, session: SessionDep) -> BookSchema:
+    new_book = BookModel(
+        title=book.title,
+        author=book.author,
+    )
+    session.add(new_book)
+    await session.commit()
+    return book
+
+
+@router.get("/books")
+async def get_books(session: SessionDep) -> list[BookGetSchema]:
+    query = select(BookModel)
+    result = await session.execute(query)
+    books = result.scalars().all()
+    print(f"{books=}")
+    return books
---
+++ src/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+engine = create_async_engine("sqlite+aiosqlite:///mydb.db", echo=True)
+
+new_async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with new_async_session() as session:
+        yield session
+
+
+class Base(DeclarativeBase):
+    pass
